5012161a93adfcee52387fa21723fb9990a7b4b1
sd2001 <im.swarnabha2001@gmail.com>
2020-06-28 20:53:09 +0530

    Fixing final bugs

--- drum_styles.py
+++ drum_styles.py
@@ -7,25 +7,25 @@ x1,y1= 10,10   #coordinates for the drums
 x2,y2=10,300
 x3,y3=465,10
 x4,y4=465,300
-
 w,h=160,160
+
 def draw(frame): 
-    cv2.rectangle(frame,(x1,y1),(x1+w,y1+h),(0,255,0),2)
+    cv2.rectangle(frame,(x1,y1),(x1+w,y1+h),(0,255,0),2)     #drawing the drums
     cv2.rectangle(frame,(x2,y2),(x2+w,y2+h),(255,255,0),2)
     cv2.rectangle(frame,(x3,y3),(x3+w,y3+w),(255,0,255),2)
     cv2.rectangle(frame,(x4,y4),(x4+w,y4+w),(245,212,66),2)
     
 def drum_press(frame,x,y,w1,h1):
-    if x>x1 and y>y1 and x+w1<x1+w and y+h1<y1+h:
+    if x>x1 and y>y1 and x+w1<x1+w and y+h1<y1+h:               #checking which drum it collided with
         cv2.rectangle(frame,(x1,y1),(x1+w,y1+h),(0,255,0),-1)
         pygame.mixer.Sound('crash_c.wav').play()
-        time.sleep(0.1)
+        time.sleep(0.08)
         pygame.mixer.Sound('crash_c.wav').stop()
             
     elif x>x2 and y>y2 and x+w1<x2+w and y+h1<y2+h:    
         cv2.rectangle(frame,(x2,y2),(x2+w,y2+h),(255,255,0),-1)
         pygame.mixer.Sound('kick02_c.wav').play()
-        time.sleep(0.1)
+        time.sleep(0.08)
         pygame.mixer.Sound('kick02_c.wav').stop()
         
         
@@ -38,7 +38,7 @@ def drum_press(frame,x,y,w1,h1):
     elif x>x4 and y>y4 and x+w1<x4+w and y+h1<y4+h:    
         cv2.rectangle(frame,(x4,y4),(x4+w,y4+h),(245,212,66),-1)
         pygame.mixer.Sound('snrbuzz_c.wav').play()
-        time.sleep(0.1)
+        time.sleep(0.08)
         pygame.mixer.Sound('snrbuzz_c.wav').stop()
         
         
