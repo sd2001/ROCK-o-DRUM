bfd73d19cd4e81c876292f3d7883336e04476662
sd2001 <im.swarnabha2001@gmail.com>
2020-06-28 20:32:02 +0530

    fixing bugs

--- drum_player.py
+++ drum_player.py
@@ -3,19 +3,19 @@ import numpy as np
 from drum_styles import draw,drum_press 
 cap=cv2.VideoCapture(1)
 while True:
-    ret,frame=cap.read()               #
+    ret,frame=cap.read()               #accessing the frames
     frame=cv2.flip(frame,1)
     #frame=cv2.GaussianBlur(frame,(9,9),0)
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #_, binary = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)
     
-    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)  #converting to Hue_Saturation_Vue format
     #mask=cv2.inRange(hsv,lower_red,upper_red)
     #kernel=np.ones((5,5),np.float32)/25
     #mask=cv2.filter2D(mask,-1,kernel)
     #mask=cv2.blur(mask,(3,3))
-    draw(frame)
-    kernel1=np.ones((4,4),np.uint8)
+    draw(frame)                                #creating the rectangular drums
+    kernel1=np.ones((4,4),np.uint8)             #kernels for smoothing the frames
     kernel2=np.ones((15,15),np.uint8)
     lower_red=np.array([136,87,111])
     upper_red=np.array([179,255,255])
