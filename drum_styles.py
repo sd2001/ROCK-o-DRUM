b634fdce82f1f3260bb65a844816b84b4c5818be
sd2001 <im.swarnabha2001@gmail.com>
2020-06-28 21:51:10 +0530

    Final Fixes(color)

--- drum_styles.py
+++ drum_styles.py
@@ -13,7 +13,7 @@ def draw(frame):
     cv2.rectangle(frame,(x1,y1),(x1+w,y1+h),(0,255,0),2)     #drawing the drums
     cv2.rectangle(frame,(x2,y2),(x2+w,y2+h),(255,255,0),2)
     cv2.rectangle(frame,(x3,y3),(x3+w,y3+w),(255,0,255),2)
-    cv2.rectangle(frame,(x4,y4),(x4+w,y4+w),(245,212,66),2)
+    cv2.rectangle(frame,(x4,y4),(x4+w,y4+w),(66,212,245),2)
     
 def drum_press(frame,x,y,w1,h1):
     if x>x1 and y>y1 and x+w1<x1+w and y+h1<y1+h:               #checking which drum it collided with
@@ -36,7 +36,7 @@ def drum_press(frame,x,y,w1,h1):
         pygame.mixer.Sound('rim_c.wav').stop()
         
     elif x>x4 and y>y4 and x+w1<x4+w and y+h1<y4+h:    
-        cv2.rectangle(frame,(x4,y4),(x4+w,y4+h),(245,212,66),-1)
+        cv2.rectangle(frame,(x4,y4),(x4+w,y4+h),(66,212,245),-1)
         pygame.mixer.Sound('snrbuzz_c.wav').play()
         time.sleep(0.08)
         pygame.mixer.Sound('snrbuzz_c.wav').stop()
